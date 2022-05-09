d847da9c0fb427872462495119ec5dd862b7bd02
Samir-SB <samir.belhadi@univ-batna2.dz>
2022-05-09 21:52:39 +0100

    create envirenemnt

--- format-dataset.py
+++ format-dataset.py
@@ -18,22 +18,23 @@ colors = ['red', 'blue', 'green', 'orange', 'pink', 'black',
           'yellow', 'brown', 'green', 'violet', 'indigo', 'teal']
 
 def listing_files(foldername):
-    data_files = []
+    list_files = []
     for root, directories, files in walk(foldername, topdown=False):
         for name in files:
             filename = join(root, name)
-            data_files.append(filename)
-    return data_files
+            list_files.append(filename)
+    return list_files
 
 
 def read_files(list_files):
-    list_dfs = []
+    list_dataframes = []
     for filename in list_files:
         df = pd.read_csv(filename, delimiter=delimiter)
         # add columns label
         df.columns = columns_label
-        list_dfs.append(df)
-    return list_dfs
+        df = preprocessing(df)
+        list_dataframes.append(df)
+    return list_dataframes
 
 
 def preprocessing(df):
@@ -47,16 +48,15 @@ def preprocessing(df):
     # combine (Lat, Long) in new column called coordinate
     df['coordinate'] = list(zip(df.Latitude, df.Longitude))
 
-    # exclude unecessary columns
+    # remove unnecessary columns
     df = df.loc[:, ~df.columns.isin(columns_excluded)]
 
     return df
 
 # FIXME #https://stackoverflow.com/questions/22483588/how-can-i-plot-separate-pandas-dataframes-as-subplots
-def plot(dfs, colors):    
-    for df, color in zip(dfs, colors):
-        df.plot(x="Longitude", y="Latitude", c=color)
-   
+def plot(list_dataframes, colors):    
+    for df, color in zip(list_dataframes, colors):
+        df.plot(x="Longitude", y="Latitude", c=color)   
 
 
 def distance_from(loc1, loc2):
@@ -67,27 +67,18 @@ def distance_from(loc1, loc2):
 
 
 def get_all_distance(dfs, df):
-    dist_df = df.rename(columns={'coordinate': 'user'})
-    # print(dist_df.)
-    #dist_df.rename(columns={'coordinate': 'user'}, inplace=True)
-    # print(dist_df.columns.values)
+    dist_df = df.rename(columns={'coordinate': 'user'})  
 
     for i in range(1, len(dfs)):
         ms_positions = []
-        # // !DELETE this
-        # print('ms length', len(dfs[i].coordinate))
-        # _MAX_LENGTH = len(df.coordinate)
-        # print(_MAX_LENGTH)
-        # return
         for loc1, loc2 in zip(df.coordinate, dfs[i].coordinate):
-            # print(loc1, ' ', loc2)
             distance = distance_from(loc1, loc2)
             ms_dict = {'distance': distance, 'position': loc2}
             ms_positions.append(ms_dict)
 
         columns_nb = len(dist_df.columns)
         dist_df.insert(columns_nb, f'ms-{columns_nb}', pd.Series(ms_positions))
-
+        
     return dist_df
 
 def plot_distance(ms):
@@ -105,30 +96,27 @@ def plot_all_distance(dfs, df):
 
 
 # Algorithm
-list_files = listing_files(foldername)
-# print(len(list_files))
+def create_env(foldername, nb_files = 10 ):
+    list_files = listing_files(foldername)
+    selected_files = list_files[:nb_files]
+    dfs = read_files(selected_files)
+    user_df = dfs[0]
+    # print(user_df.head())
+    df = get_all_distance(dfs, user_df)
+    return df
 
-selected_files = list_files[:10]
-dfs = read_files(selected_files)
-# print('length: ',len(dfs))
-# print(dfs[0].head())
+df = create_env(foldername)
 
+df.to_csv('illinois-env.csv', sep='|')
 
-for index, df in enumerate(dfs):
-    dfs[index] = preprocessing(df)
-# print(dfs[0].head())
-# plot(dfs, colors)
-
-user_df = dfs[0]
-print(user_df.head())
-
-df = get_all_distance(dfs, user_df)
-print(df.iloc[0])
+# print(df.iloc[0])
 # plot_all_distance(dfs, df, plot_distance)
 ms = df['ms-1']
-state = df.iloc[100]
-df_transposed = pd.DataFrame(list(state.T[1:]))
-# print( df_transposed)
+
+state = df.iloc[150]
+print(state.T)
+df_transposed = pd.DataFrame(list(state[1:].T))
+print(df_transposed)
 
 df_transposed[["Longitude", "Latitude"]] = pd.DataFrame(df_transposed.position.tolist(), index= df_transposed.index)
 print(df_transposed)
@@ -138,7 +126,6 @@ df_transposed.plot.scatter(x="Longitude", y="Latitude", c="blue")
 
 ms_trajectory = pd.DataFrame(list(ms)).position
 plot_distance(ms)
-# plot_trajectory(ms_trajectory)
-# plt.show()
+plt.show()
 
 #https://pandas.pydata.org/pandas-docs/version/0.25.0/reference/api/pandas.DataFrame.plot.scatter.html
